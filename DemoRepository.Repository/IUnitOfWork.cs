2cac850fd8f4261a23f3d9e4dc6b57814e842e76
picfiesta <picfiesta.git@gmail.com>
2014-12-22 22:49:15 +0530

    new interface
    Autogenerated columns set to false and added IUnitOfWork interface

---
+++ DemoRepository.Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DemoRepository.Repository
+{
+    public interface IUnitOfWork
+    {
+        void Dispose();
+        void Dispose(bool disposing);
+        RepositoryBase<T> Repository<T>() where T : DemoRepository.Entity.BaseEntity;
+        void Save();
+    }
+}
