8be20e7a9ebfc6bce780850e1f532fc653633740
picfiesta <onlinegit@gmail.com>
2015-02-02 23:50:04 +0530

    Angular-First Implementation
    Usage of webapi with angular

---
+++ DemoRepository.UI/Scripts/app.js
@@ -0,0 +1,20 @@
+﻿(function(){var app = angular.module('testApp', []);
+
+    app.controller('TestController', function($scope, $http){
+        
+        $scope.orders = [];
+        $scope.message = 'Hello';
+        var responsePromise = $http.get("http://localhost:2326/api/Orders");
+
+        responsePromise.success(function (data, status, headers, config) {
+            console.log('Data retrieved with ' + data.length + ' records');
+            $scope.orders = data;
+            console.log('data:' + $scope.orders.length)
+
+        });
+        responsePromise.error(function (data, status, headers, config) {
+            alert("AJAX failed!");
+        });
+    });
+
+})();
